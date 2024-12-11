1b3fb53b5c1e7115f859a805bf0e49191e7e44b8
Kaar Joseph <kaarunkuch@gmail.com>
2024-12-11 17:06:24 -0500

    Actualización MAIN

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 from src.menu import Menu
 from src.inventario import Inventario
+import multiprocessing
 
 def main():
     print("=== SISTEMA DE GESTIÓN DE PEDIDOS ===")
     print("Simulación de un restaurante usando procesamiento paralelo.\n")
-    
-    # Crear una instancia única del inventario
+
+    # Crear el inventario inicial
     inventario = Inventario()
-    
-    # Instancia del menú principal
-    menu = Menu(inventario)
+
+    # Crear el Manager para listas compartidas
+    manager = multiprocessing.Manager()
+
+    # Inicializar el menú con el inventario y el Manager
+    menu = Menu(inventario, manager)
+
+    # Mostrar el menú principal
     menu.mostrar_menu()
 
 if __name__ == "__main__":
